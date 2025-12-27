63de757c6c99b54c230a87e475a5ce1dc95497d0
Kali <michaeljang94@gmail.com>
2025-12-28 00:59:24 +1300

    Waiting game

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -17,6 +17,8 @@ import { jwtDecode } from 'jwt-decode';
 import { TokenPayload } from './pages/PlayerProfilePage';
 import { useAuth } from './components/auth/AuthProvider';
 
+import './pages/game/GamePage.css';
+
 interface SessionInfo {
   session_id: string;
   table_name: string;
@@ -126,8 +128,13 @@ export const GamePage: React.FC = () => {
     <>
       <Container maxWidth="sm">
         <Grid container spacing={2}>
-          <Grid size={12}>
-            {!inGame && <h1>Game</h1>}
+          <Grid
+            size={12}
+            display={inGame ? 'flow' : 'flex'}
+            alignContent={'center'}
+            justifyContent="center"
+          >
+            {!inGame && <h1>Waiting...</h1>}
             {inGame && <h1>{sessionInfo?.table_name}</h1>}
             {inGame && <h3>{tableInfo?.game}</h3>}
             {/* {inGame && <h4>{sessionInfo?.session_id}</h4> } */}
@@ -137,8 +144,44 @@ export const GamePage: React.FC = () => {
             {inGame && <LinearProgress color="warning" />}
           </Grid>
           {!inGame && (
-            <Grid size={12}>
-              <h1>Not in game</h1>
+            <Grid size={12} display="flex" alignContent={'center'} justifyContent="center">
+              <div className="main">
+                <div className="dog">
+                  <div className="dog__paws">
+                    <div className="dog__bl-leg leg">
+                      <div className="dog__bl-paw paw"></div>
+                      <div className="dog__bl-top top"></div>
+                    </div>
+                    <div className="dog__fl-leg leg">
+                      <div className="dog__fl-paw paw"></div>
+                      <div className="dog__fl-top top"></div>
+                    </div>
+                    <div className="dog__fr-leg leg">
+                      <div className="dog__fr-paw paw"></div>
+                      <div className="dog__fr-top top"></div>
+                    </div>
+                  </div>
+
+                  <div className="dog__body">
+                    <div className="dog__tail"></div>
+                  </div>
+
+                  <div className="dog__head">
+                    <div className="dog__snout">
+                      <div className="dog__nose"></div>
+                      <div className="dog__eyes">
+                        <div className="dog__eye-l"></div>
+                        <div className="dog__eye-r"></div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="dog__head-c">
+                    <div className="dog__ear-l"></div>
+                    <div className="dog__ear-r"></div>
+                  </div>
+                </div>
+              </div>
             </Grid>
           )}
           {inGame && <Grid size={12}></Grid>}
